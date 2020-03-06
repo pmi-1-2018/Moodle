9527a5d033f6d1673f07f478523b85fc451933f3
Erik Mirai <erik.mirai@outlook.com>
2020-03-06 11:04:52 +0200

    Teacher class added

---
+++ Moodle/Classes/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moodle.Classes
+{
+   public class Teacher : User
+    {
+        public int FacultyID { get; }
+
+        public Teacher(int id, string name, string surname, string password, string login, int facultyID) : base(id, name, surname, password, login)
+        {
+            FacultyID = facultyID;
+        }
+    }
+}
