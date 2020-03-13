0b9a7b0010d6047285f9be9cfe59f73b527fdda6
Erik Mirai <erik.mirai@outlook.com>
2020-03-13 16:36:36 +0200

    Question class added

---
+++ Moodle/Classes/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moodle.Classes
+{
+    public class Question
+    {
+        public string Content { get; }
+
+        public IEnumerable<Answer> Answers { get; }
+
+        public double Mark { get; }
+
+        public Question(string content, IEnumerable<Answer> answers, double mark)
+        {
+            this.Content = content;
+            this.Answers = answers;
+            this.Mark = mark;
+        }
+    }
+}
