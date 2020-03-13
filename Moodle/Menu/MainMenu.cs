6135c5e0e675c93c031baeec4ae3889272e56934
S1NED <denic.poruchnik@gmail.com>
2020-03-13 18:39:05 +0200

    Created display menu funtio function

---
+++ Moodle/Menu/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moodle.Menu
+{
+    class MainMenu
+    {
+        //static void Main(string[] args)
+        //{
+        //    bool showMenu = true;
+        //    while (showMenu)
+        //    {
+        //        showMenu = DisplayMenu();
+        //    }
+        //}
+
+        public static bool DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Moodle!");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Log in ");
+            Console.WriteLine("2) Option 2");
+            Console.WriteLine("3) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    // Log in function here
+                    return true;
+                case "2":
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+    }
+}
--- Moodle/Program.cs
+++ Moodle/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using static Moodle.Menu.MainMenu;
 
 namespace Moodle
 {
     class Program
     {
+        //static void Main(string[] args)
+        //{
+        //     Console.WriteLine("Hello World!");
+
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //var menu = new MainMenu();
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                displayMenu = DisplayMenu();
+            }
         }
+
     }
 }
