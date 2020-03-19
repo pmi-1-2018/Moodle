9bb9537442e84eb0b475b2eb9ca46f7c9c4278d6
Denys Poruchnyk <43414527+S1NEDIO@users.noreply.github.com>
2020-03-19 15:49:27 +0200

    Modified menu

--- Moodle/Menu/MainMenu.cs
+++ Moodle/Menu/MainMenu.cs
@@ -12,18 +12,27 @@ namespace Moodle.Menu
             Console.WriteLine("Welcome to the Moodle!");
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1) Log in ");
-            Console.WriteLine("2) Option 2");
-            Console.WriteLine("3) Exit");
+            Console.WriteLine("2) Sign un");
+            Console.WriteLine("3) Test");
+            Console.WriteLine("3) Download test");
+            Console.WriteLine("5) Exit");
             Console.Write("\r\nSelect an option: ");
 
             switch (Console.ReadLine())
             {
                 case "1":
-                    // Log in function here
+                    // Log in function
                     return true;
                 case "2":
+                    // Sign up function
                     return true;
                 case "3":
+                    //Test function
+                    return true;
+                case "4":
+                    //Download test function
+                    return true;
+                case "5":
                     return false;
                 default:
                     return true;
