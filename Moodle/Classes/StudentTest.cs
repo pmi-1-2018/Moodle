5e8c2424c74a560b7f5641774c9b8da163cffaed
Erik Mirai <erik.mirai@outlook.com>
2020-03-22 21:17:25 +0200

    Student Test added

---
+++ Moodle/Classes/StudentTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moodle.Classes
+{
+    public class StudentTest
+    {
+        public IList<Question> ListOfQuestions { get; }
+        public string Author { get; }
+        public int ID { get; private set; }
+        public string Name { get; }
+        public DateTime Date { get; }
+        public double MinResult { get; }
+        public bool CanRetake { get; }
+        public void SetID(int id)
+        {
+            ID = id;
+        }
+        public StudentTest(int id, string name, string author, DateTime dateTime, IList<Question> listOfQuestions, double minResult, bool canRetake)
+        {
+            ID = id;
+            this.Name = name;
+            this.Author = author;
+            this.Date = dateTime;
+            this.ListOfQuestions = listOfQuestions;
+            this.CanRetake = canRetake;
+            this.MinResult = minResult;
+        }
+
+    }
+}
