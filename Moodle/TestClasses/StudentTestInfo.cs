dabfcb59d566a7e1ff34547adabfb66fd7b12f7a
Erik Mirai <erik.mirai@outlook.com>
2020-03-27 21:27:40 +0200

    class StudentTestInfo added

---
+++ Moodle/TestClasses/StudentTestInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moodle.TestClasses
+{
+    public class StudentTestInfo
+    {
+        public int Mark { get; }
+        public string TestName { get; }
+        public int StudentID { get; }
+        public StudentTestInfo(int studentId, string testName, int mark)
+        {
+            StudentID = studentId;
+            TestName = testName;
+            Mark = mark;
+        }
+    }
+}
