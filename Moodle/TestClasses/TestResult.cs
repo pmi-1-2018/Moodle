4d0f3fc28587e9e47fd8657d1876b5cecabf4869
Erik Mirai <erik.mirai@outlook.com>
2020-03-27 21:29:23 +0200

    class TestResult added

---
+++ Moodle/TestClasses/TestResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moodle.TestClasses
+{
+    public class TestResult
+    {
+        public int Number { get; set; }
+        public bool IsRight { get; set; }
+    }
+}
